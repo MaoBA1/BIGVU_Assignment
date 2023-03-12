54b2de3a1937574a213e6c36aa2515c9d60c52dc
Maor Bar Asher <94516860+MaoBA1@users.noreply.github.com>
2023-03-12 11:43:45 +0200

    123

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const compression = require('compression');
+const path = require('path');
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+console.log('server started on port:',PORT);
+app.listen(PORT);
